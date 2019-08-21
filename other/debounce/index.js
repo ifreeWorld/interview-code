9b551cde26cc5d01e3cafd4b743ba8267ea69a22
WZL <853370316@qq.com>
2019-08-21 23:10:08 +0800

    提交

--- other/debounce/index.js
+++ other/debounce/index.js
@@ -1,4 +1,4 @@
-// 防抖
+// 简易版本防抖
 function debounce(func, context, params, delay) {
   var timer = null
   return () => {
@@ -15,20 +15,57 @@ window.onmousemove = debounce((a) => {
   console.log(a)
 }, window, [1], 500)
 
+// 加强版防抖
+function debounce(func, delay, immediate) {
+  var timer = null
+  var context
+  var args
+  var later = () => {
+    return setTimeout(() => {
+      timer = null
+      if (!immediate) {
+        func.apply(context, args)
+      }
+    }, delay)
+  }
 
-// 节流
-function throttle(func, context, params, delay) {
-  var last = 0
-  return () => {
-    var now = Date.now()
-    if (now - last > delay) {
-      last = now
-      setTimeout(() => {
-        func.apply(context, params)
-      }, delay)
+  return (...params) => {
+    if (timer) {
+      clearTimeout(timer)
+      timer = later()
+    } else {
+      timer = later()
+      if (immediate) {
+        func.apply(context, args)
+      } else {
+        context = this
+        args = params
+      }
     }
   }
 }
+
+window.onmousemove = debounce(() => {
+  console.log(1111)
+}, 500, true)
+
+// 节流
+function throttle(func, delay) {
+  var last = 0
+  var timer = null
+  return (...params) => {
+    var now = new Date()
+    if (now - last > delay) {
+      last = now
+      timer = setTimeout(() => {
+        func.apply(this, params)
+      })
+    }
+  }
+}
+window.onmousemove = throttle(() => {
+  console.log(1111)
+}, 500)
 
 // 第二种节流
 function throttle(func, context, params, delay) {
