f90d9c8730453832284b518d03385ed904a45634
wangzilong <wangzilong@qianxin.com>
2020-04-27 15:50:17 +0800

    提交代码

---
+++ other/get-tree/index.js
@@ -0,0 +1,141 @@
+/**
+ * 
+ * 关系型数组转换成树形结构对象 
+  var obj = [
+    { id:3, parent:2 },
+    { id:1, parent:null },
+    { id:2, parent:1 },
+  ]
+  o = {
+    obj: {
+      id: 1,
+      parent: null,
+      child: {
+        id: 2,
+        parent: 1,
+        child: {
+            id: 3
+            parent: 2
+        }
+      }
+    }
+  }
+ * 
+ */
+var obj = {}
+function getTree(arr) {
+  func(arr, null, obj)
+  console.log(JSON.stringify(obj))
+}
+
+function func(arr, parentId, obj) {
+  arr.forEach(item => {
+    if (item.parent === parentId) {
+      if (parentId === null) {
+        obj = {
+          ...item
+        }
+      } else {
+        obj.child = {
+          ...item
+        }
+      }
+      func(arr, item.id, obj.child)
+    }
+  })
+  return obj
+}
+
+// 第二种非递归做法
+getTree([
+  { id: 3, parent: 2 },
+  { id: 1, parent: null },
+  { id: 2, parent: 1 }
+])
+
+var arr = [
+  { id: 3, parent: 2 },
+  { id: 1, parent: null },
+  { id: 2, parent: 1 }
+]
+
+function getTree(arr) {
+  var map = {}
+  var result = {}
+  arr.forEach(item => {
+    map[item.id] = item
+  })
+  
+  arr.forEach(item => {
+    if (item.parent) {
+      var parent = map[item.parent]
+      parent.child = item
+    } else {
+      result.obj = item
+    }
+  })
+  return result
+}
+
+console.log(JSON.stringify(getTree(arr)))
+
+
+/**
+ * 关系型数组转换成树形结构对象，children为数组
+ */
+var arr = [
+  {
+    id: 1,
+    pid: 0,
+    name: 'body'
+  },
+  {
+    id: 2,
+    pid: 1,
+    name: 'title'
+  },
+  {
+    id: 3,
+    pid: 1,
+    name: 'div'
+  },
+  {
+    id: 4,
+    pid: 3,
+    name: 'span'
+  },
+  {
+    id: 5,
+    pid: 3,
+    name: 'icon'
+  },
+  {
+    id: 6,
+    pid: 4,
+    name: 'subspan'
+  }
+]
+
+function getTree(arr) {
+  var result = []
+  var map = {}
+  
+  arr.forEach(item => {
+    map[item.id] = item
+  })
+  
+  arr.forEach(item => {
+    var parent = map[item.pid]
+    if (!parent) {
+      result.push(item)
+    } else {
+      if (!parent.children) {
+        parent.children = []
+      }
+      parent.children.push(item)
+    }
+  })
+  return result
+}
+
+console.log(JSON.stringify(getTree(arr)))
--- other/responsive/index.js
+++ other/responsive/index.js
@@ -1,26 +1,47 @@
 /**
  * 实现一个简单的响应式
  */
+function Observer(a) {
+  for (var key in a) {
+    debugger
+    if (a.hasOwnProperty(key)) {
+      Object.defineProperty(a, key, {
+        get: () => {
+          console.log('get')
+          return a[key]
+        },
+        set: (val) => {
+          console.log('set')
+          a[key] = val
+        }
+      })
+    }
+  }
+}
 var a = {
   b: 1,
   c: 2
 }
-function responsive() {
-  Object.keys(a).forEach(key => {
-    var value = a[key]
-    Object.defineProperty(a, key, {
-      get: () => {
-        console.log('get', key)
-        return value
-      },
-      set: (val) => {
-        console.log('set', key, val)
-        value = val
-      }
-    })
-  })
-}
-responsive()
+Observer(a)
 
 var b = a.b
 a.c = 3
+
+
+var a = {
+  name: '112'
+}
+
+var proxy = new Proxy(a, {
+  get: function(target, key, receiver) {
+    return target[key]
+  },
+  set: function(target, key, value, receiver) {
+    target[key] = value
+  }
+});
+console.log(proxy.name)
+
+proxy.name = '23'
+console.log(proxy.name)
+console.log(a.name)
